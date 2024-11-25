d35bc93a56b26057be5f6aeddf6fa86845d32883
Oscar Li <learning@oscarhkli.com>
2024-11-25 17:08:50 +0000

    feat: init MahjongScoreCalculator

---
+++ src/main/java/com/oscarhkli/mahjong/score/MahjongScoreCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.oscarhkli.mahjong.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongScoreCalculatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MahjongScoreCalculatorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/oscarhkli/mahjong/score/MahjongScoreCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarhkli.mahjong.score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongScoreCalculatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
