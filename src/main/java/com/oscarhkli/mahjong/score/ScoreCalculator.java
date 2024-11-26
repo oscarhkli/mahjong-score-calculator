ff8eb125b79bceb5713d2886b036d1076892d124
Oscar Li <learning@oscarhkli.com>
2024-11-26 17:47:13 +0000

    feat: refine logic to calculate Common Hand

--- src/main/java/com/oscarhkli/mahjong/score/ScoreCalculator.java
+++ src/main/java/com/oscarhkli/mahjong/score/ScoreCalculator.java
@@ -1,7 +1,9 @@
 package com.oscarhkli.mahjong.score;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,75 +15,210 @@ public class ScoreCalculator {
     }
   }
 
-  public int calculateTiles(List<String> tiles) {
-    var sortedTiles = tiles.stream().sorted().toList();
-    var used = new boolean[tiles.size()];
-    var validTriplets = new ArrayList<List<String>>();
-    calculateCommonHand(sortedTiles, 0, new ArrayList<>(), validTriplets, used);
+  public int calculate(List<String> tiles) {
+    var characters = new ArrayList<String>();
+    var bamboos = new ArrayList<String>();
+    var dots = new ArrayList<String>();
+    var winds = new ArrayList<String>();
+    var dragons = new ArrayList<String>();
 
-    log.info("Valid triplets: {}", validTriplets);
-    if (validTriplets.size() == 4) {
-      String first = null;
-      for (var i = 0; i < used.length; i++) {
-        if (!used[i]) {
-          if (first == null) {
-            first = tiles.get(i);
-          } else {
-            log.info("first: {}, second: {}", first, tiles.get(i));
-            return isPair(first, tiles.get(i)) ? 1 : 0;
-          }
-        }
+    for (var tile : tiles) {
+      if ("Red".equals(tile) || "Green".equals(tile) || "White".equals(tile)) {
+        dragons.add(tile);
+      } else if ("East".equals(tile)
+          || "South".equals(tile)
+          || "West".equals(tile)
+          || "North".equals(tile)) {
+        winds.add(tile);
+      } else if (tile.startsWith("C")) {
+        characters.add(tile);
+      } else if (tile.startsWith("B")) {
+        bamboos.add(tile);
+      } else {
+        dots.add(tile);
       }
     }
+
+    var characterGroupedTiles = construct("Character", characters);
+    var bambooGroupedTiles = construct("Bamboo", bamboos);
+    var dotGroupedTiles = construct("Dot", dots);
+    if (isCommonHand(characterGroupedTiles, bambooGroupedTiles, dotGroupedTiles)) {
+      return 1;
+    }
+
     return 0;
   }
 
-  private boolean isPair(String a, String b) {
-    return a.equals(b);
+  private boolean isCommonHand(
+      GroupedTiles characterGroupedTiles,
+      GroupedTiles bambooGroupedTiles,
+      GroupedTiles dotGroupedTiles) {
+    if (characterGroupedTiles.getMelds().size() == 4) {
+      return false;
+    }
+    if (bambooGroupedTiles.getMelds().size() == 4) {
+      return false;
+    }
+    if (dotGroupedTiles.getMelds().size() == 4) {
+      return false;
+    }
+    if (characterGroupedTiles.getMelds().size()
+            + bambooGroupedTiles.getMelds().size()
+            + dotGroupedTiles.getMelds().size()
+        != 4) {
+      return false;
+    }
+
+    var unusedCharacterTileCounts = new int[10];
+    for (var unusedTile : characterGroupedTiles.getUnusedTiles()) {
+      unusedCharacterTileCounts[unusedTile.charAt(1) - '0']++;
+    }
+    var characterEye = 0;
+    for (var i = 1; i <= 9; i++) {
+      if (unusedCharacterTileCounts[i] != 0 && unusedCharacterTileCounts[i] != 2) {
+        return false;
+      }
+      if (unusedCharacterTileCounts[i] == 2) {
+        if (characterEye != 0) {
+          return false;
+        }
+        characterEye = i;
+      }
+    }
+    var unusedBambooTileCounts = new int[10];
+    for (var unusedTile : bambooGroupedTiles.getUnusedTiles()) {
+      unusedBambooTileCounts[unusedTile.charAt(1) - '0']++;
+    }
+    var bambooEye = 0;
+    for (var i = 1; i <= 9; i++) {
+      if (unusedBambooTileCounts[i] != 0 && unusedBambooTileCounts[i] != 2) {
+        return false;
+      }
+      if (unusedBambooTileCounts[i] == 2) {
+        if (bambooEye != 0) {
+          return false;
+        }
+        bambooEye = i;
+      }
+    }
+    var unusedDotTileCounts = new int[10];
+    for (var unusedTile : dotGroupedTiles.getUnusedTiles()) {
+      unusedDotTileCounts[unusedTile.charAt(1) - '0']++;
+    }
+    var dotEye = 0;
+    for (var i = 1; i <= 9; i++) {
+      if (unusedDotTileCounts[i] != 0 && unusedDotTileCounts[i] != 2) {
+        return false;
+      }
+      if (unusedDotTileCounts[i] == 2) {
+        if (dotEye != 0) {
+          return false;
+        }
+        dotEye = i;
+      }
+    }
+
+    return characterEye > 0 && bambooEye == 0 && dotEye == 0
+        || characterEye == 0 && bambooEye > 0 && dotEye == 0
+        || characterEye == 0 && bambooEye == 0 && dotEye > 0;
   }
 
-  private boolean isSameTileAndConsecutive(String a, String b) {
-    return a.charAt(0) == b.charAt(0) && a.charAt(1) + 1 == b.charAt(1);
+  @Value
+  public static class GroupedTiles {
+    String type;
+    List<List<String>> melds;
+    List<String> unusedTiles;
   }
 
-  private void calculateCommonHand(
-      List<String> tiles,
-      int index,
-      List<Pair<String, Integer>> subset,
-      List<List<String>> validTriplets,
-      boolean[] used) {
-    // Validate
-    for (var selected : subset) {
-      if (used[selected.value]) {
-        return;
+  public GroupedTiles construct(String type, List<String> tiles) {
+    if (tiles.isEmpty()) {
+      return new GroupedTiles(type, List.of(), List.of());
+    }
+    var tileCounts = new int[10];
+    for (var tile : tiles) {
+      tileCounts[tile.charAt(1) - '0']++;
+    }
+    var groupedTilesCandidates = new ArrayList<GroupedTiles>();
+    groupedTilesCandidates.add(
+        constructGroupedTiles(type, Arrays.copyOf(tileCounts, tileCounts.length), List.of()));
+    for (var i = 1; i <= 9; i++) {
+      if (tileCounts[i] >= 2) {
+        var adjustedTileCounts = Arrays.copyOf(tileCounts, tileCounts.length);
+        adjustedTileCounts[i] -= 2;
+        groupedTilesCandidates.add(constructGroupedTiles(type, adjustedTileCounts, List.of(i, i)));
       }
     }
-    if (subset.size() == 2) {
-      if (!isSameTileAndConsecutive(subset.get(0).key(), subset.get(1).key())) {
-        return;
-      }
-    } else if (subset.size() == 3) {
-      if (!isSameTileAndConsecutive(subset.get(1).key(), subset.get(2).key())) {
-        return;
-      }
-      // Add to validTriplets
-      var triplet = new ArrayList<String>();
-      for (var selected : subset) {
-        log.info("add to valid triplet: {}", selected);
-        triplet.add(selected.key());
-        used[selected.value()] = true;
-      }
-      validTriplets.add(triplet);
-      return;
-    }
-    // Backtracking
-    for (var i = index; i < tiles.size(); i++) {
-      if (used[i]) {
-        continue;
-      }
-      subset.add(new Pair<>(tiles.get(i), i));
-      calculateCommonHand(tiles, i + 1, subset, validTriplets, used);
-      subset.removeLast();
-    }
+    log.debug("groupedTilesCandidates: {}", groupedTilesCandidates);
+    return deduceBestGroupedTiles(groupedTilesCandidates);
   }
+
+  private GroupedTiles deduceBestGroupedTiles(List<GroupedTiles> groupedTilesCandidates) {
+    if (groupedTilesCandidates.size() == 1) {
+      return groupedTilesCandidates.getFirst();
+    }
+    GroupedTiles bestGroupTilesCandidate = null;
+    var maxMeldsSize = -1;
+    var maxUnusedPairs = -1;
+    for (var current : groupedTilesCandidates) {
+      var currentMeldSize = current.getMelds().size();
+      var unusedTileCounts = new int[10];
+      for (var unusedTile : current.getUnusedTiles()) {
+        unusedTileCounts[unusedTile.charAt(1) - '0']++;
+      }
+      var currentUnusedPairs = 0;
+      for (var unusedTileCount : unusedTileCounts) {
+        if (unusedTileCount >= 2) {
+          currentUnusedPairs++;
+        }
+      }
+      if (currentMeldSize > maxMeldsSize) {
+        bestGroupTilesCandidate = current;
+        maxMeldsSize = currentMeldSize;
+        maxUnusedPairs = currentUnusedPairs;
+      } else if (currentMeldSize == maxMeldsSize && currentUnusedPairs > maxUnusedPairs) {
+        bestGroupTilesCandidate = current;
+        maxUnusedPairs = currentUnusedPairs;
+      }
+    }
+    return bestGroupTilesCandidate;
+  }
+
+  private GroupedTiles constructGroupedTiles(
+      String type, int[] tileCounts, List<Integer> reservedTiles) {
+    var validMelds = new ArrayList<List<Integer>>();
+    for (var i = 1; i <= 7; i++) {
+      while (tileCounts[i] > 0) {
+        if (tileCounts[i + 1] == 0 || tileCounts[i + 2] == 0) {
+          break;
+        }
+        validMelds.add(List.of(i, i + 1, i + 2));
+        tileCounts[i]--;
+        tileCounts[i + 1]--;
+        tileCounts[i + 2]--;
+      }
+    }
+
+    var prefix = type.charAt(0);
+    var melds =
+        validMelds.stream()
+            .map(
+                validMeld ->
+                    validMeld.stream()
+                        .map(tileValue -> String.format("%s%d".formatted(prefix, tileValue)))
+                        .toList())
+            .toList();
+    var unusedTiles = new ArrayList<String>();
+    for (var i = 1; i <= 9; i++) {
+      for (var j = 0; j < tileCounts[i]; j++) {
+        unusedTiles.add(String.format("%s%d", prefix, i));
+      }
+    }
+    for (var reservedTile : reservedTiles) {
+      unusedTiles.add(String.format("%s%d", prefix, reservedTile));
+    }
+    unusedTiles.sort(String::compareTo);
+
+    return new GroupedTiles(type, melds, unusedTiles);
+  }
+
 }
--- src/test/java/com/oscarhkli/mahjong/score/ScoreCalculatorTest.java
+++ src/test/java/com/oscarhkli/mahjong/score/ScoreCalculatorTest.java
@@ -2,44 +2,129 @@ package com.oscarhkli.mahjong.score;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import com.oscarhkli.mahjong.score.ScoreCalculator.GroupedTiles;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ScoreCalculatorTest {
 
   ScoreCalculator scoreCalculator = new ScoreCalculator();
 
+  @ParameterizedTest
+  @MethodSource
+  void constructGroupedType(List<String> tiles, String type, List<GroupedTiles> expected) {
+    var groupedTiles = scoreCalculator.construct(type, tiles);
+    then(groupedTiles).usingRecursiveComparison().isIn(expected);
+  }
+
+  private static Stream<Arguments> constructGroupedType() {
+    return Stream.of(
+        Arguments.of(
+            List.of("D1", "D2", "D3"),
+            "Dot",
+            List.of(new GroupedTiles("Dot", List.of(List.of("D1", "D2", "D3")), List.of()))),
+        Arguments.of(
+            List.of("D1", "D1", "D2", "D3", "D3"),
+            "Dot",
+            List.of(
+                new GroupedTiles("Dot", List.of(List.of("D1", "D2", "D3")), List.of("D1", "D3")))),
+        Arguments.of(
+            List.of(
+                "D1", "D1", "D2", "D2", "D2", "D3", "D3", "D3", "D3", "D4", "D4", "D5", "D5", "D5"),
+            "Dot",
+            List.of(
+                new GroupedTiles(
+                    "Dot",
+                    List.of(
+                        List.of("D1", "D2", "D3"),
+                        List.of("D1", "D2", "D3"),
+                        List.of("D2", "D3", "D4"),
+                        List.of("D3", "D4", "D5")),
+                    List.of("D5", "D5")))),
+        Arguments.of(
+            List.of("D1", "D1", "D2", "D2", "D2", "D3", "D3", "D3", "D4", "D4", "D5", "D5", "D5"),
+            "Dot",
+            List.of(
+                new GroupedTiles(
+                    "Dot",
+                    List.of(
+                        List.of("D1", "D2", "D3"),
+                        List.of("D1", "D2", "D3"),
+                        List.of("D2", "D3", "D4")),
+                    List.of("D4", "D5", "D5", "D5")))),
+        Arguments.of(
+            List.of("D1", "D2", "D2", "D2", "D3", "D3", "D4", "D5"),
+            "Dot",
+            List.of(
+                new GroupedTiles(
+                    "Dot",
+                    List.of(List.of("D1", "D2", "D3"), List.of("D3", "D4", "D5")),
+                    List.of("D2", "D2")))),
+        Arguments.of(
+            List.of("D1", "D2", "D2", "D2", "D3", "D3", "D4", "D5", "D5"),
+            "Dot",
+            List.of(
+                new GroupedTiles(
+                    "Dot",
+                    List.of(List.of("D1", "D2", "D3"), List.of("D3", "D4", "D5")),
+                    List.of("D2", "D2", "D5")),
+                new GroupedTiles(
+                    "Dot",
+                    List.of(List.of("D1", "D2", "D3"), List.of("D2", "D3", "D4")),
+                    List.of("D2", "D5", "D5")))),
+        Arguments.of(
+            List.of("D1", "D2", "D2", "D2", "D3", "D3", "D4", "D5", "D5", "D5", "D5"),
+            "Dot",
+            List.of(
+                new GroupedTiles(
+                    "Dot",
+                    List.of(List.of("D1", "D2", "D3"), List.of("D3", "D4", "D5")),
+                    List.of("D2", "D2", "D5", "D5", "D5")))),
+        Arguments.of(
+            List.of("D1", "D3", "D5"),
+            "Dot",
+            List.of(new GroupedTiles("Dot", List.of(), List.of("D1", "D3", "D5")))),
+        Arguments.of(List.of(), "Dot", List.of(new GroupedTiles("Dot", List.of(), List.of()))));
+  }
+
   @Test
   @DisplayName("Test Common Hand without winds, dragons nor bonus tiles")
   void testCommonHand() {
-    var tiles = List.of("D1", "D2", "D3", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "C5", "C6", "D5", "D5");
-    var score = scoreCalculator.calculateTiles(tiles);
+    var tiles =
+        List.of("D1", "D2", "D3", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "C5", "C6", "D5", "D5");
+    var score = scoreCalculator.calculate(tiles);
     then(score).isEqualTo(1);
   }
 
   @Test
   @DisplayName("Test Common Hand with duplicate tiles")
   void testCommonHandWithDuplicateTiles() {
-    var tiles = List.of("D1", "D2", "D3", "D2", "D3", "D4", "C1", "C2", "C3", "C4", "C5", "C6", "D5", "D5");
-    var score = scoreCalculator.calculateTiles(tiles);
+    var tiles =
+        List.of("D1", "D2", "D3", "D2", "D3", "D4", "C1", "C2", "C3", "C4", "C5", "C6", "D5", "D5");
+    var score = scoreCalculator.calculate(tiles);
     then(score).isEqualTo(1);
   }
 
   @Test
   @DisplayName("Test Common Hand with smaller eyes")
   void testCommonHandWithSmallerEyes() {
-    var tiles = List.of("D1", "D2", "D2", "D2", "D3", "D3", "D4", "D5", "C1", "C2", "C3", "C4", "C5", "C6");
-    var score = scoreCalculator.calculateTiles(tiles);
+    var tiles =
+        List.of("D1", "D2", "D2", "D2", "D3", "D3", "D4", "D5", "C1", "C2", "C3", "C4", "C5", "C6");
+    var score = scoreCalculator.calculate(tiles);
     then(score).isEqualTo(1);
   }
 
   @Test
   @DisplayName("Test Invalid Common Hand")
   void testNonCommonHand() {
-    var tiles = List.of("D1", "D2", "D3", "B1", "B2", "B4", "C1", "C2", "C3", "C4", "C5", "C6", "D5", "D5");
-    var score = scoreCalculator.calculateTiles(tiles);
-    then(score).isEqualTo(0);
+    var tiles =
+        List.of("D1", "D2", "D3", "B1", "B2", "B4", "C1", "C2", "C3", "C4", "C5", "C6", "D5", "D5");
+    var score = scoreCalculator.calculate(tiles);
+    then(score).isEqualTo(1);
   }
-
 }
